a796cd4becb12864cdb3af8a0c9dd04d6eab15ea
William Salazar <williamyenn@gmail.com>
2024-01-07 16:36:19 -0400

    segundo commit

---
+++ resources/views/Backend/layouts/app.blade.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    @include('Backend.layouts.common-head')
+</head>
+
+<body id="page-top">
+
+    <!-- Page Wrapper -->
+    <div id="wrapper">
+
+        @include('Backend.layouts.sidebar')
+
+        <!-- Content Wrapper -->
+        <div id="content-wrapper" class="d-flex flex-column">
+
+            <!-- Main Content -->
+            <div id="content">
+
+                @include('Backend.layouts.header')
+
+                @yield('main-content')
+
+
+            </div>
+            <!-- End of Main Content -->
+
+            @include('Backend.layouts.footer')
+
+        </div>
+        <!-- End of Content Wrapper -->
+
+    </div>
+    <!-- End of Page Wrapper -->
+
+    @include('Backend.layouts.common-end')
+    @stack('custom-scripts')
+
+</body>
+
+</html>
